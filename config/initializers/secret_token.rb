b0debec4644a7c9942368abc97e5ecbb88b7f991
Marcos Martins <marcos.martins2307@gmail.com>
2014-02-04 08:49:00 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dance::Application.config.secret_token = '8b560cdf02b1267ce22105dd791c7f09bee8a6148013faea063c920a7c282533ea64dd4ff96e601dee52225ca7302356a060530a60c88d21b23ad29389c953b6'
